1df0cf02ebc6724876b6ab47c6bab5d3098bc5a6
trideb-religh <panigrahitrideb1@gmail.com>
2023-07-06 11:42:35 +0530

    updated

--- frontend/src/components/common/footer/Footer.jsx
+++ frontend/src/components/common/footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './footer.css'
+import { blog } from '../dummydata/dummyData'
 
 const Footer = () => {
   return (
@@ -16,6 +17,83 @@ const Footer = () => {
                 </div>
             </div>
         </section>
+        <footer>
+            <div className="container padding">
+                <div className="box logo">
+                    <h1>MYSCHOOL</h1>
+                    <span>online education & learing</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, est.</p>
+                    <i className='fab fa-facebook-f icon'></i>
+                    <i className='fab fa-instagram icon'></i>
+                    <i className='fab fa-twitter icon'></i>
+                </div>
+                <div className="box link">
+                    <h3>Explore</h3>
+                    <ul>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                    </ul>
+                </div>
+                <div className="box link">
+                    <h3>Quick Links</h3>
+                    <ul>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                        <li>About Us</li>
+                    </ul>
+                </div>
+                <div className="box">
+                    <h3>Recent Post</h3>
+                    {
+                        blog.slice(0,3).map((data)=>{
+                            return(
+                                <div className="items flexSB">
+                                <div className="img">
+                                    <img src={data.cover} alt="" />
+                                </div>
+                                <div className="text">
+                                    <span>
+                                        <i className="fa fa-user"></i>
+                                        <label htmlFor="">{data.type}</label>
+                                    </span>
+                                    <span>
+                                        <i className="fa fa-calendar-alt"></i>
+                                        <label htmlFor="">{data.date}</label>
+                                    </span>
+                                    <h4>{data.agenda}</h4>
+                                </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <div className="box last">
+                    <h3>Have a Questions?</h3>
+                    <ul>
+                        <li>
+                            <i className="fa fa-map"></i>
+                            203 Fake ST.Mount View , san francisio california , USA
+                        </li>
+                        <li>
+                            <i className="fa fa-phone-alt"></i>
+                            +91-8249713049
+                        </li>
+                        <li>
+                            <i className="fa fa-paper-plane"></i>
+                            panigrahitrideb1@gmail.com
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+        <div className="legal">
+            <p>Copyright @2023 All rights reserved</p>
+        </div>
     </>
   )
 }
